23d0831987487b98813acec4045f4becc464e9aa
mooncode163 <chyfemail163@163.com>
2021-07-21 11:44:01 +0800

    ui

--- cleanbuild.py
+++ cleanbuild.py
@@ -12,7 +12,7 @@ import socket
  
 
 def cleanbuild():
-    listdir = ["game/unityLibrary/build","game/launcher/build","game/imageselector/build","game/unityLibrary/src/main/assets","game/unityLibrary/src/main/jniLibs","game/.idea"]
+    listdir = ["AppCommon/app/build","AppCommon/LibCommon/build","AppCommon/imageselector/build","AppCommon/.idea"]
     for dir in listdir:
         flag = os.path.exists(dir)
         if flag: 
